f1163d707e3f31b40ad193048a23a83a97f8761e
unknown <rivero.florencio.marco@iescamas.es>
2026-02-23 14:25:49 +0100

    Creamos la clase Triangulo

--- code/src/main/java/com/endes/figuras/Cuadrado.java
+++ code/src/main/java/com/endes/figuras/Cuadrado.java
@@ -20,18 +20,21 @@ this.esquina = esquina;
 /**
 * Calculamos el perimetro del cuadrado.
 */
+@Override
 public double perimetro() {
 return 4 * lado;
 }
 /**
 * Calculamos el area del cuadrado.
 */
+@Override
 public double area() {
 return lado * lado;
 }
 /**
 * Rotamos el cuadrado mediante una serie de ecuasiones.
 */
+@Override
 public void rotar(double angulos) {
 double radianes = Math.toRadians(angulos);
 double cos = Math.cos(radianes);
@@ -47,6 +50,7 @@ esquina.setY(nuevoY);
 /**
 * Trasladamos el cuadrado del presicion.
 */
+@Override
 public void trasladar(double dx, double dy) {
 double nuevoX = esquina.getX() + dx;
 double nuevoY = esquina.getY() + dy;
